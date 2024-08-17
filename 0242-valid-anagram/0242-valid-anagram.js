7d9ac65ac30f5f6293e79ca6ef3dded5b32ebc7a
Pangho <161326510+Pangho04@users.noreply.github.com>
2024-08-17 22:09:05 +0900

    Time: 191 ms (5.04%), Space: 56.6 MB (5.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,14 +5,16 @@
  */
 var isAnagram = function (s, t) {
   let sExeptT = s;
+  let parsedT = t;
 
   for (let i = 0; i < t.length; i++) {
     if (sExeptT.includes(t[i])) {
       sExeptT = sExeptT.replace(t[i], "");
+      parsedT = parsedT.replace(t[i], "")
     }
   }
 
-  if (sExeptT === "") {
+  if (sExeptT.length === 0 && parsedT.length === 0) {
     return true;
   }
 
