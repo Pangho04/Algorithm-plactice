088f6e27dfcb7c6792b3b483e37ef02368e26435
Pangho <161326510+Pangho04@users.noreply.github.com>
2024-08-18 17:23:49 +0900

    Time: 57 ms (52.64%), Space: 51.9 MB (74.29%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,6 +4,11 @@
  * @return {number}
  */
 var search = function (nums, target) {
+
+  if(!nums.includes(target)){
+    return -1;
+  }
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === target) {
       return i;
