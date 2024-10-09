0c43bdaa41db5614815bf8331c24ce346d44d129
Pangho <161326510+Pangho04@users.noreply.github.com>
2024-10-09 17:32:03 +0900

    Time: 95 ms (60.2%), Space: 58.8 MB (31.97%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.ts
@@ -0,0 +1,43 @@
+function findAnagrams(s: string, p: string): number[] {
+  const pMap = new Map();
+
+  for (const str of p) {
+    pMap.set(str, (pMap.get(str) || 0) + 1);
+  }
+
+  const compareMap = new Map();
+  let left = 0;
+  let right = 0;
+  let sameChars = 0;
+  const result = [];
+
+  while (right < s.length) {
+    compareMap.set(s[right], (compareMap.get(s[right]) || 0) + 1);
+
+    if(compareMap.get(s[right]) === pMap.get(s[right])){
+      sameChars++;
+    }
+
+    if (right - left + 1 === p.length) {
+      if(sameChars === pMap.size){
+        result.push(left);
+      }
+
+      if(compareMap.get(s[left]) === pMap.get(s[left])){
+        sameChars--;
+      }
+
+      compareMap.set(s[left], compareMap.get(s[left]) - 1);
+
+      if(compareMap.get(s[left]) === 0) {
+        compareMap.delete(s[left]);
+      }
+
+      left++;
+    }
+
+    right++;
+  }
+
+  return result;
+};
